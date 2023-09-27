6e12b15ce8c40bfe398b52852751d0561ab8c514
LinguisGeek <143286677+LinguisGeek@users.noreply.github.com>
2023-09-27 09:37:37 +0200

    Update detail.js

--- detail.js
+++ detail.js
@@ -83,11 +83,10 @@ function loadTrainingDetails() {
                 while (detailDiv.firstChild) {
                     detailDiv.removeChild(detailDiv.firstChild);
                 }
-
-                trainings.forEach(training => {
+                trainings.forEach((training, index) => {  // Notez l'argument `index` ajouté ici
                     // Vérifiez chaque élément `training` ici
                     if (training) {
-                        displayTrainingDetail(training);
+                        displayTrainingDetail(training, index);  // Et passez l'`index` ici
                         console.log('calling displayTrainingDetail from loadTrainingDetails');
                     } else {
                         console.error('Invalid training data: ', training);
@@ -104,12 +103,13 @@ function loadTrainingDetails() {
 
 
 
-
-function displayTrainingDetail(training) {
+function displayTrainingDetail(training, index) {
+    console.log('Index avant la création du bouton:', index);
     console.log('Fonction displayTrainingDetail appelée avec les données suivantes :', training);
     const container = document.querySelector('.training-container');
     const trainingDiv = document.createElement('div');
     trainingDiv.className = 'training-card';
+    trainingDiv.innerHTML += `<button class="edit-button" data-index="${index}">Modifier</button>`; 
 
     // Vérifiez si les propriétés sont définies avant de les utiliser
     if (training && Object.keys(training).length > 0) {
@@ -139,17 +139,54 @@ function displayTrainingDetail(training) {
 }
 
 
+function openEditModal(index) {
+    console.log(index);
+    // Convertit l'index en un nombre, si ce n'est pas déjà le cas
+    index = Number(index);
+    console.log('Index converti :', index);  // Affiche l'index converti dans la console
+    document.getElementById("trainingIndex").value = index;  // stockez l'index dans un champ caché
+
+    db.collection("TrainingDetails").doc(trainingId).get().then((doc) => {
+        if (doc.exists) {
+            const data = doc.data();
+
+            // Vérifie si l'index est dans la plage valide
+            if (index >= 0 && index < data.trainings.length) {
+                const training = data.trainings[index];
+                if (training) {
+                    // Remplir les champs de la modale avec les détails existants
+                    document.getElementById("name").value = training.name;
+                    document.getElementById("series").value = training.series;
+                    document.getElementById("recovery").value = training.recovery;
+                    document.getElementById("weight").value = training.weight;
+                    // ... et d'autres champs si nécessaire
+
+                    // Ouvrez la modale
+                    modal.style.display = "block";
+                } else {
+                    console.error('Invalid training data: ', training);
+                }
+            } else {
+                console.error('Index out of range:', index);
+            }
+        } else {
+            console.log("No such document!");
+        }
+    }).catch((error) => {
+        console.log("Error getting document:", error);
+    });
+}
 
 
 
-function saveTrainingDetails(trainingId, newTraining) {
-    return new Promise((resolve, reject) => {  // Ajout d'une nouvelle Promesse
+function saveTrainingDetails(trainingId, newTraining, index) {
+    return new Promise((resolve, reject) => {
         if (isSaving) {
             console.warn('Save operation in progress, ignoring this save request.');
-            reject('Save operation in progress');  // Rejet de la promesse si une opération de sauvegarde est déjà en cours
+            reject('Save operation in progress');
             return;
         }
-        isSaving = true;  // Set the flag to true when starting a save operation
+        isSaving = true;
 
         db.collection("TrainingDetails").doc(trainingId).get().then(doc => {
             let trainings = [];
@@ -157,23 +194,26 @@ function saveTrainingDetails(trainingId, newTraining) {
                 const data = doc.data();
                 trainings = data.trainings || [];
             }
-            trainings.push(newTraining);  // Add the new training to the array
-            
-            // Si le document existe, mettez-le à jour. Sinon, créez un nouveau document.
-            if (doc.exists) {
-                return db.collection("TrainingDetails").doc(trainingId).update({ trainings });  // Update the document
+
+            if (typeof index !== 'undefined' && index < trainings.length) {
+                // Mettez à jour l'objet existant si l'index est fourni
+                trainings[index] = newTraining;
             } else {
-                return db.collection("TrainingDetails").doc(trainingId).set({ trainings });  // Create a new document
+                // Si l'index n'est pas fourni, ajoutez le nouvel entraînement à la liste
+                trainings.push(newTraining);
             }
+
+            // Mettez à jour le document avec la nouvelle liste d'entraînements
+            return db.collection("TrainingDetails").doc(trainingId).set({ trainings });
         }).then(() => {
             console.log("Document successfully updated!");
             loadTrainingDetails();
-            resolve();  // Résolution de la promesse après la mise à jour réussie du document
+            resolve();
         }).catch((error) => {
             console.error("Error updating document: ", error);
-            reject(error);  // Rejet de la promesse en cas d'erreur
+            reject(error);
         }).finally(() => {
-            isSaving = false;  // Reset the flag when the save operation is complete
+            isSaving = false;
         });
     });
 }
@@ -188,37 +228,47 @@ document.addEventListener("DOMContentLoaded", function() {
     const form = document.getElementById("detail-form");
     form.addEventListener("submit", function(e) {
         e.preventDefault();
+        const index = document.getElementById("trainingIndex").value;
         const image = document.getElementById("image").files[0];
         const name = document.getElementById("name").value;
         const series = document.getElementById("series").value;
         const recovery = document.getElementById("recovery").value;
         const weight = document.getElementById("weight").value;
+        
+        let newTraining = { name, series, recovery, weight };  // Créez un objet newTraining ici
 
-        if (trainingId) {
-            // Upload image to Firebase Storage if provided
-            if (image) {
-                const storageRef = firebase.storage().ref();
-                const imageRef = storageRef.child(`images/${trainingId}/${image.name}`);
-                imageRef.put(image).then((snapshot) => {
-                    snapshot.ref.getDownloadURL().then((downloadURL) => {
-                        saveTrainingDetails(trainingId, {
-                            image: downloadURL,
-                            name,
-                            series,
-                            recovery,
-                            weight
-                        }).then(() => {
-                            closeModal();  // fermez la modale après la sauvegarde réussie
-                        });
+        if (!trainingId) {
+            trainingId = db.collection("TrainingDetails").doc().id; // Générez un nouveau doc ID s'il n'en existe pas
+        }
+        // Upload image to Firebase Storage if provided
+        if (image) {
+            const storageRef = firebase.storage().ref();
+            const imageRef = storageRef.child(`images/${trainingId}/${image.name}`);
+            imageRef.put(image).then((snapshot) => {
+                snapshot.ref.getDownloadURL().then((downloadURL) => {
+                    newTraining.image = downloadURL;  // Ajoutez l'URL de l'image à newTraining
+                    console.log("Data to be saved: ", newTraining);  // Log the data to be saved
+                    saveTrainingDetails(trainingId, newTraining, index).then(() => {  // passez l'index à saveTrainingDetails
+                        closeModal();
                     });
                 });
-            } else {
-                saveTrainingDetails(trainingId, { name, series, recovery, weight })
-                .then(() => {
-                    closeModal();  // fermez la modale après la sauvegarde réussie
-                });
-            }
+            });
+        } else {
+            console.log("Data to be saved: ", newTraining);  // Log the data to be saved
+            saveTrainingDetails(trainingId, newTraining, index === '' ? undefined : index).then(() => {
+                closeModal();
+            });  // <-- Ajout de la parenthèse fermante ici
         }
     });
 });
 
+
+
+
+document.addEventListener('click', function(e) {
+    if(e.target && e.target.classList.contains('edit-button')) {
+        const index = e.target.getAttribute('data-index');
+        console.log('Index récupéré de data-index:', index);  // Ajoutez cette ligne pour vérifier la valeur de l'index
+        openEditModal(index);
+    }
+});  // Ici, vous aviez besoin de fermer la fonction et l'écouteur d'événements
